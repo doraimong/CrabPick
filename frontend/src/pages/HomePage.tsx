6992c0f83495956110b81884dc72b303ab2245dc
leeseunghan <seunghan4769@naver.com>
2023-03-08 15:09:52 +0900

    [front] 컴포넌트 작성중

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import MainLayout from "../layout/MainLayout";
 import Home from "../components/MainHome/Home";
 import TopRankGame from "../components/MainHome/TopRankGame";
+import RecommendGame from "../components/MainHome/RecommendGame";
 const Main = () => {
   return (
     <MainLayout>
       <Home />
+      <RecommendGame />
       <TopRankGame />
     </MainLayout>
   );
