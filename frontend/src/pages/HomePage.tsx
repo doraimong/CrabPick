58f58fcffa88175f1ccc7734f19ad7fed73511ff
leeseunghan <seunghan4769@naver.com>
2023-03-08 10:42:56 +0900

    [front] 컴포넌트 작성

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainLayout from "../layout/MainLayout";
-import Home from "../components/Home";
+import Home from "../components/MainHome/Home";
+import TopRankGame from "../components/MainHome/TopRankGame";
 const Main = () => {
   return (
     <MainLayout>
       <Home />
+      <TopRankGame />
     </MainLayout>
   );
 };
