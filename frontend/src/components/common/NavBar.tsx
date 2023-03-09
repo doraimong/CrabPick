cfca91c78ff95487cd90e2b558da3a43b38e8433
WChan <baewc95@gmail.com>
2023-03-09 17:11:04 +0900

    [front] nav

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -15,28 +15,33 @@ const MenuBar = () => {
     "https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/01/22/j0CUgPfqy6Fn637784817551514147.jpg";
 
   const loginHandler = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault()
+    e.preventDefault();
     setLogin(true);
   };
 
   return (
     <div className={styles.navBar}>
-      <img src={logo} alt="" onClick={home} className={styles.logo} />
+      <div className={styles.navLogo}>
+        <img src={logo} alt="" onClick={home} className={styles.logo} />
+      </div>
       <div className={styles.links}>
         <Link to="/mypage/:nickName">마이페이지</Link>
-
-        <Link to="/gamebti">GameBTI</Link>
-
-        {/* 로그인X -> 로그인 링크 /  로그인 O -> 프로필 사진 */}
-        {/* {login? <img>프로필 사진</img> : <Link to="/signin">로그인</Link> } */}
-        {login? (
-          <img src={defaultImg} />
-        ) : (
-          <a onClick={loginHandler} >
-            로그인
-          </a>
-        )}
       </div>
+      <div className={styles.links}>
+        <Link to="/gamebti">GameBTI</Link>
+      </div>
+      {/* 로그인X -> 로그인 링크 /  로그인 O -> 프로필 사진 */}
+      {/* {login? <img>프로필 사진</img> : <Link to="/signin">로그인</Link> } */}
+      {login ? (
+        <div className={styles.navProfile}>
+          <></>
+          <img src={defaultImg}/>
+        </div>
+      ) : (
+        <div className={styles.linkSignIn}>
+          <a onClick={loginHandler}>로그인</a>
+        </div>
+      )}
     </div>
   );
 };
