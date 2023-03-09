0c19af4b7e0d1f86c3965ae9533831e56e34f895
leeseunghan <seunghan4769@naver.com>
2023-03-09 11:24:54 +0900

    [front] layout css

--- frontend/src/components/gamebti/Gamebti.tsx
+++ frontend/src/components/gamebti/Gamebti.tsx
@@ -3,9 +3,7 @@ import styles from "./Gamebti.module.css";
 
 const Gamebti = () => {
   return (
-    <div className={styles.gamebti}>
-      <h1>여기는 겜비티아이 게임을 실행할겁니다.</h1>
-    </div>
+    <div className={styles.gamebti}>여기는 겜비티아이 게임을 실행할겁니다.</div>
   );
 };
 
