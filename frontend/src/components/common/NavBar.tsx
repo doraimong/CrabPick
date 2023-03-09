d27ae47d85ee76676a461a624cb6fac541b692a8
leeseunghan <seunghan4769@naver.com>
2023-03-09 17:08:25 +0900

    [front] layout

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import logo from "../../asset/logo.png";
@@ -15,7 +15,7 @@ const MenuBar = () => {
     "https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/01/22/j0CUgPfqy6Fn637784817551514147.jpg";
 
   const loginHandler = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault()
+    e.preventDefault();
     setLogin(true);
   };
 
@@ -23,16 +23,17 @@ const MenuBar = () => {
     <div className={styles.navBar}>
       <img src={logo} alt="" onClick={home} className={styles.logo} />
       <div className={styles.links}>
-        <Link to="/mypage/:nickName">마이페이지</Link>
-
+        <Link to="/game-news">게임 뉴스</Link>
+        <Link to="/sale-info">할인 정보</Link>
         <Link to="/gamebti">GameBTI</Link>
+        {/* <Link to="/mypage/:nickName">마이페이지</Link> */}
 
         {/* 로그인X -> 로그인 링크 /  로그인 O -> 프로필 사진 */}
         {/* {login? <img>프로필 사진</img> : <Link to="/signin">로그인</Link> } */}
-        {login? (
-          <img src={defaultImg} />
+        {login ? (
+          <img src={defaultImg} alt="" />
         ) : (
-          <a onClick={loginHandler} >
+          <a onClick={loginHandler} href="/">
             로그인
           </a>
         )}
