070daeec6fd18b446c72523a7414cc2da69bae53
WChan <baewc95@gmail.com>
2023-03-09 14:38:55 +0900

    [front] nav-bar | 미완

--- frontend/src/components/auth/SignInForm.tsx
+++ frontend/src/components/auth/SignInForm.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SignInForm.module.css";
 const SignInForm = () => {
+  const [login, setLogin] = useState<Boolean>(false);
+
+  const loginHandler = () => {};
+
   return (
     <div className={styles.form}>
       <h1>로그인 폼입니다.</h1>
+      <button onClick={loginHandler}>로그인</button>
     </div>
   );
 };
