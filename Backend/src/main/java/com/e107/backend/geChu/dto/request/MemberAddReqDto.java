071f580e4b7cfddcd83f523df31bae7132bf4839
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-03-10 11:00:22 +0900

    crud 작업중

--- Backend/src/main/java/com/e107/backend/geChu/dto/request/MemberAddReqDto.java
+++ Backend/src/main/java/com/e107/backend/geChu/dto/request/MemberAddReqDto.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class AddMemberReqDto {
+public class MemberAddReqDto {
 
     private String name;
     private String email;
---
+++ Backend/src/main/java/com/e107/backend/geChu/dto/response/MemberRespDto.java
@@ -0,0 +1,38 @@
+package com.e107.backend.geChu.dto.response;
+
+import com.e107.backend.geChu.domain.entity.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class MemberRespDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String steamToken;
+    private String steamNickname;
+    private List<String> friends;
+    private List<BookmarkRespDto> bookmarks = new ArrayList<>();
+    private List<SteamLibraryRespDto> steamLibraries = new ArrayList<>();
+
+
+//    public static MemberRespDto of(Member m) {
+//        return MemberRespDto.builder()
+//                .id(m.getId())
+//                .name(m.getName())
+//                .email(m.getEmail())
+//                .steamToken(m.getSteamToken())
+//                .steamNickname(m.getSteamNickname())
+//                .friends(m.getFriends())
+//
+//    }
+}
