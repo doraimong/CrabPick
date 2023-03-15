46c57fd06fb16760c740bc6b908fac88fee45406
강동훈 <asddiin@gmail.com>
2023-03-15 12:06:14 +0900

    review 자동화코드 수정

--- data_crawling/steam_reviews.py
+++ data_crawling/steam_reviews.py
@@ -5,11 +5,18 @@ from urllib import parse
 
 import requests
 
+roooooot = os.path.dirname(__file__)
+
 try:
-    with open(f"{os.path.dirname(__file__)}/games.csv", "r", newline='', encoding="utf-8") as game_f:
-        with open(f"{os.path.dirname(__file__)}/reviews.csv", "w", newline='', encoding="utf-8") as review_f:
-            if os.path.exists(f"{os.path.dirname(__file__)}/error_log2.txt"):
-                    with open(f"{os.path.dirname(__file__)}/error_log2.txt", "r", encoding="utf-8") as log:
+    if not os.path.exists(f"{roooooot}/reviews.csv"):
+        with open(f"{roooooot}/reviews.csv", "a", newline='', encoding="utf-8") as games:
+            csv_writer = csv.writer(games)
+            csv_writer.writerow([None,"game_id", "review_id", "author", "voted_up", "language", "steam_purchase"])
+
+    with open(f"{roooooot}/games.csv", "r", newline='', encoding="utf-8") as game_f:
+        with open(f"{roooooot}/reviews.csv", "w", newline='', encoding="utf-8") as review_f:
+            if os.path.exists(f"{roooooot}/error_log2.txt"):
+                    with open(f"{roooooot}/error_log2.txt", "r", encoding="utf-8") as log:
                         log_lines = log.readlines()
                         games_line, review_line = map(int, log_lines[-1].split())
             else:
@@ -17,7 +24,7 @@ try:
                 games_line = 1
             games = csv.reader(game_f)
             review_csv = csv.writer(review_f)
-            review_csv.writerow([None,"game_id", "review_id", "author", "voted_up", "language", "steam_purchase"])
+            # review_csv.writerow([None,"game_id", "review_id", "author", "voted_up", "language", "steam_purchase"])
             query = {
                 "json": 1,    
                 "num_per_page": 100,    
@@ -33,7 +40,12 @@ try:
                         start = time.time()
                         r = requests.get(url)
                         if r.status_code == 200:
-                            result = r.json()
+                            try:
+                                result = r.json()
+                            except:
+                                with open(f"{roooooot}/error_log2.txt", "a", encoding="utf-8") as f:
+                                    f.writelines([f"JSON Decode Error {game[1]}"])
+                                continue
                             # print(result)
                             if result["success"] != 1:
                                 break
@@ -55,11 +67,11 @@ try:
                                 query["cursor"] = result["cursor"]
                 games_line += 1
 except KeyboardInterrupt as e:
-        with open(f"{os.path.dirname(__file__)}/error_log2.txt", "a", encoding="utf-8") as f:
+        with open(f"{roooooot}/error_log2.txt", "a", encoding="utf-8") as f:
             f.writelines(["Manualy shutdown" + "\n", f"{games_line} {review_line}", "\n"])
             print(e)
 except Exception as e:
-    with open(f"{os.path.dirname(__file__)}/error_log2.txt", "a", encoding="utf-8") as f:
+    with open(f"{roooooot}/error_log2.txt", "a", encoding="utf-8") as f:
         f.writelines([e.__str__() + "\n", f"{games_line} {review_line}", "\n"])
         print(e)
     
