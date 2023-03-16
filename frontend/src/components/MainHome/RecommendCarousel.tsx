f18acb21ec8beb649a8596ae67c9400d93e3a228
leeseunghan <seunghan4769@naver.com>
2023-03-16 15:10:40 +0900

    [front] 추천게임 캐러셀

--- frontend/src/components/MainHome/RecommendCarousel.tsx
+++ frontend/src/components/MainHome/RecommendCarousel.tsx
@@ -1,15 +1,45 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Slider, { Settings } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import nextArrow from "../../asset/nextarrow.png";
+
 import styles from "./RecommendCarousel.module.css";
 
 interface Props {
-  images: { url: string; description: string[] }[];
+  games: {
+    url: string;
+    title: string;
+    id: number;
+    genre: string;
+    etc: string;
+  }[];
+}
+function SampleNextArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
 }
 
-const Carousel: React.FC<Props> = ({ images }) => {
+function SamplePrevArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+const Carousel: React.FC<Props> = ({ games }) => {
+  const navigate = useNavigate();
   const [currentSlide, setCurrentSlide] = useState<number>(0);
 
   const settings: Settings = {
@@ -21,23 +51,27 @@ const Carousel: React.FC<Props> = ({ images }) => {
     cssEase: "linear",
     slidesToShow: 1, // 한 캐러셀에 보여질 컨텐츠 수
     slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
 
   return (
-    <div>
-      <Slider {...settings}>
-        {images.map((image, index) => (
-          <div key={index} className={styles.carousel}>
-            <img src={image.url} alt={`Slide ${index}`} />
-            <ul>
-              {image.description.map((desc, idx) => (
-                <li key={idx}>{desc}</li>
-              ))}
-            </ul>
+    <Slider {...settings}>
+      {games.map((game, index) => (
+        <div
+          key={index}
+          className={styles.carousel}
+          onClick={() => navigate(`/detail/${game.id}`)}
+        >
+          <img src={game.url} alt={`Slide ${index}`} className={styles.image} />
+          <div>
+            <h1>{game.title}</h1>
+            <h4>{game.genre}</h4>
+            <h4>{game.etc}</h4>
           </div>
-        ))}
-      </Slider>
-    </div>
+        </div>
+      ))}
+    </Slider>
   );
 };
 
