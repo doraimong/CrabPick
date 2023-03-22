57ea063ab506b846b48d623d83a1f9bb4b62063c
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-03-22 15:06:55 +0900

    CORS 해제

--- Backend/src/main/java/com/e107/backend/geChu/controller/GameController.java
+++ Backend/src/main/java/com/e107/backend/geChu/controller/GameController.java
@@ -9,16 +9,14 @@ import com.e107.backend.geChu.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/game")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class GameController {
 
     private final GameService gameService;
