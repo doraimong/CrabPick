3d27b0d20067eae774c8a71338720a7cdcfdc5a6
WChan <baewc95@gmail.com>
2023-03-23 15:05:05 +0900

    [front] etc

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -6,13 +6,20 @@ import styles from "./Page.module.css";
 import SearchLayout from "../layout/SearchLayout";
 import SearchResult from "../components/search/SearchResult";
 import { useLocation } from "react-router-dom";
-const SearchPage = () => {
-    const { state } = useLocation()
+
+
+const SearchPage = (gameList: any) => {
+  const { state } = useLocation();
+  const filteredgame = gameList.gameList.filter((itemList:any) => {
+    return itemList.name.toUpperCase().includes(state.toUpperCase())
+  })
   return (
     <SearchLayout>
       <div className={styles.page}>
         검색 : {state}
-        <SearchResult />
+        {filteredgame.map((game: any, i: number) => (
+          <SearchResult key={i} game={game} />
+        ))}
       </div>
     </SearchLayout>
   );
