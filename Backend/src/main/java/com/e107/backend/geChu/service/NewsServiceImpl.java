ef016e904aaeeed7385f1b8b6315184b64e6ef78
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-03-27 15:31:28 +0900

    뉴스 스케쥴러 기능 구현

--- Backend/src/main/java/com/e107/backend/geChu/service/NewsServiceImpl.java
+++ Backend/src/main/java/com/e107/backend/geChu/service/NewsServiceImpl.java
@@ -5,9 +5,14 @@ import com.e107.backend.geChu.domain.repository.NewsRepository;
 import com.e107.backend.geChu.dto.response.NewsRespDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,8 +32,36 @@ public class NewsServiceImpl implements NewsService{
         return resp;
     }
 
-    // 뉴스업데이트 기능 추가 필요
-    public void updateNews(){
-
+    // 뉴스업데이트 기능 추가 완료
+    @Override
+    public void updateNews() throws IOException {
+        String url;
+        int j = 0;
+        for (int i = 1; i <= 2; i++) {
+            url = "https://www.gamemeca.com/news.php?p=" + i;
+            Document doc = Jsoup.connect(url).get();
+            Elements elem = doc.select("ul[class=\"list_news\"] li");
+            for (Element e : elem) {
+                Elements s = e.select("strong[class=\"tit_thumb_h\"]");
+                Elements s1 = e.select("strong[class=\"tit_thumb\"]");
+                if (s.hasText()) {
+                    News news = newsRepository.findById(j + 1L).orElseThrow();
+                    String subject = s.text();
+                    String imageLink = e.select("a img").attr("src");
+                    String link = "https://www.gamemeca.com" + s.select("a").attr("href");
+                    String date = e.select("div[class=\"day_news\"]").text();
+                    news.setAttribute(subject, link, imageLink, date);
+                }
+                if (s1.hasText()) {
+                    News news = newsRepository.findById(j + 1L).orElseThrow();
+                    String subject = s1.text();
+                    String imageLink = e.select("a img").attr("src");
+                    String link = "https://www.gamemeca.com" + s1.select("a").attr("href");
+                    String date = e.select("div[class=\"day_news\"]").text();
+                    news.setAttribute(subject, link, imageLink, date);
+                }
+                j++;
+            }
+        }
     }
 }
