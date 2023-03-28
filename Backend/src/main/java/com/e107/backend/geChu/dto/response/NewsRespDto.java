4e2e59e8223af0ed9ca61694a63ffc304ca7b2de
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-03-28 12:03:12 +0900

    news dto 수정

--- Backend/src/main/java/com/e107/backend/geChu/dto/response/NewsRespDto.java
+++ Backend/src/main/java/com/e107/backend/geChu/dto/response/NewsRespDto.java
@@ -12,12 +12,16 @@ public class NewsRespDto {
     Long id;
     String subject;
     String url;
+    String imageLink;
+    String date;
 
     public static NewsRespDto of(News n) {
         return NewsRespDto.builder()
                 .id(n.getId())
                 .subject(n.getSubject())
                 .url(n.getUrl())
+                .imageLink(n.getImageLink())
+                .date(n.getDate())
                 .build();
     }
 
