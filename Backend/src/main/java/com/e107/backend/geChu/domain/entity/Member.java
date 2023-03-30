9cb617397905877309095a8ce029a5ea65395094
박종현 <pjh6947@naver.com>
2023-03-30 14:20:53 +0900

    user Entity 수정 되돌리기 / application.yml 에 mysql 설정

--- Backend/src/main/java/com/e107/backend/geChu/domain/entity/Member.java
+++ Backend/src/main/java/com/e107/backend/geChu/domain/entity/Member.java
@@ -1,6 +1,5 @@
 package com.e107.backend.geChu.domain.entity;
 
-import com.e107.backend.geChu.memberJwt.entity.Authority;
 import lombok.*;
 import org.hibernate.annotations.Comment;
 
@@ -8,7 +7,6 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Getter
@@ -20,35 +18,16 @@ public class Member {
     @GeneratedValue
     private Long id;
 
-//    @Comment("유저 이름")
-//    private String name;
-//
-//    @Email
-//    @Comment("유저 이메일")
-//    private String email;
-//
-//    @Comment("스팀 토큰")
-//    private String steamToken;
+    @Comment("유저 이름")
+    private String name;
 
-///////////////////////////////
-    //!!personname 사용
-    @Column(name = "username", length = 50, unique = true)
-    private String username;
+    @Email
+    @Comment("유저 이메일")
+    private String email;
 
-    //!!key 사용
-    @Column(name = "password", length = 100)
-    private String password;
+    @Comment("스팀 토큰")
+    private String steamToken;
 
-    @Column(name = "activated")
-    private boolean activated;
-
-    @ManyToMany
-    @JoinTable(
-            name = "user_authority",
-            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
-            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
-    private Set<Authority> authorities;
-////////////////////////////////
     @Comment("스팀 닉네임")
     private String steamNickname;
 
