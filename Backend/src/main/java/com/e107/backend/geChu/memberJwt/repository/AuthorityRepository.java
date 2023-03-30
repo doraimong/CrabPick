7c3dc154f227589707a3b65510747b61cec7200a
박종현 <pjh6947@naver.com>
2023-03-30 17:55:43 +0900

    로그인 이사 완료[기능 정상 동작]

--- Backend/src/main/java/com/e107/backend/geChu/memberJwt/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.e107.backend.geChu.memberJwt.repository;
-
-import com.e107.backend.geChu.memberJwt.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
---
+++ Backend/src/main/java/com/e107/backend/geChu/security/service/CustomUserDetailsService.java
@@ -0,0 +1,52 @@
+package com.e107.backend.geChu.security.service;
+
+import com.e107.backend.geChu.security.entity.User;
+import com.e107.backend.geChu.security.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component("userDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+   private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
+   private final UserRepository userRepository;
+
+   //!! UserRepository를 주입받는다.
+   public CustomUserDetailsService(UserRepository userRepository) {
+      this.userRepository = userRepository;
+   }
+
+   @Override
+   @Transactional
+   public UserDetails loadUserByUsername(final String username) {
+      logger.debug("-loadUserByUsername(final String username)");
+      //!! 로그인 시 DB에서 유저정보, 권한정보를 가져오게 된다.-> 해당 정보를 기반으로 userdetails.User를 생성하여 반환한다.(아래 코드 내용 포함)
+      return userRepository.findOneWithAuthoritiesByUsername(username)
+         .map(user -> createUser(username, user))
+         .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+   }
+
+   private org.springframework.security.core.userdetails.User createUser(String username, User user) {
+      logger.debug("-createUser(String username, User user)");
+      if (!user.isActivated()) {
+         throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
+      }
+
+      List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
+              .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+              .collect(Collectors.toList());
+
+      return new org.springframework.security.core.userdetails.User(user.getUsername(),
+              user.getPassword(),
+              grantedAuthorities);
+   }
+}
