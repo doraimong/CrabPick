b1ba76ce6a21414d47bc0ed0003ed2629a4d0c3d
박종현 <pjh6947@naver.com>
2023-03-30 12:19:57 +0900

    파일 이사(오류x) / member 요소 user로 일부 대체

---
+++ Backend/src/main/java/com/e107/backend/geChu/memberJwt/dto/AuthorityDto.java
@@ -0,0 +1,12 @@
+package com.e107.backend.geChu.memberJwt.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+   private String authorityName;
+}
--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -12,3 +12,16 @@ insert into game_comment values (1,'내용', '2013-08-05 18:19:03', 1, 1)
 
 --                 ID  AUTHOR_ID    CONTENT  IS_RECOMMEND  LANGUAGE  PLAYTIME_ALL PLAYTIME_AT_REVIEW  VOTES_UP
 insert into review values (1, 1, 'CONTNET', true, 'korean', 123.5, 5.5, 33)
+
+
+-- jwt test user - 수정해야
+-- insert into "user" (username, password, nickname, activated) values ('admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1);
+-- insert into "user" (username, password, nickname, activated) values ('user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1);
+--
+-- insert into authority (authority_name) values ('ROLE_USER');
+-- insert into authority (authority_name) values ('ROLE_ADMIN');
+--
+-- insert into user_authority (user_id, authority_name) values (1, 'ROLE_USER');
+-- insert into user_authority (user_id, authority_name) values (1, 'ROLE_ADMIN');
+-- insert into user_authority (user_id, authority_name) values (2, 'ROLE_USER');
+--
