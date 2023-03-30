b0bf1cc066cae25b1c5c6eb9c6690b2a51e956dd
박종현 <pjh6947@naver.com>
2023-03-30 10:07:36 +0900

    jwt 코드 옮겨오고, 에러 정상화

---
+++ Backend/src/main/java/com/e107/backend/geChu/memberJwt/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.e107.backend.geChu.memberJwt.controller;
+
+import com.e107.backend.geChu.memberJwt.controller.dto.MemberResponseDto;
+import com.e107.backend.geChu.memberJwt.service.MemberService;
+import com.e107.backend.geChu.memberJwt.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponseDto> findMemberInfoById() {
+        return ResponseEntity.ok(memberService.findMemberInfoById(SecurityUtil.getCurrentMemberId()));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<MemberResponseDto> findMemberInfoByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(memberService.findMemberInfoByEmail(email));
+    }
+}
---
+++ Backend/src/main/java/com/e107/backend/geChu/memberJwt/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.e107.backend.geChu.memberJwt.repository;
+
+import com.e107.backend.geChu.memberJwt.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+}
