248d0ab3be5e95d1248d562c320ba6d2b4d46710
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-04-03 17:54:27 +0900

    코멘트기능 작성,조회

---
+++ Backend/src/main/java/com/e107/backend/geChu/dto/request/CommentReqDto.java
@@ -0,0 +1,15 @@
+package com.e107.backend.geChu.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CommentReqDto {
+    private String content;
+
+}
+
--- Backend/src/main/java/com/e107/backend/geChu/service/CommentServiceImpl.java
+++ Backend/src/main/java/com/e107/backend/geChu/service/CommentServiceImpl.java
@@ -1,12 +1,17 @@
 package com.e107.backend.geChu.service;
 
 import com.e107.backend.geChu.domain.entity.Comment;
+import com.e107.backend.geChu.domain.entity.Game;
+import com.e107.backend.geChu.domain.entity.Member;
 import com.e107.backend.geChu.domain.repository.CommentRepository;
+import com.e107.backend.geChu.domain.repository.GameRepository;
+import com.e107.backend.geChu.domain.repository.MemberRepository;
 import com.e107.backend.geChu.dto.response.CommentRespDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +22,8 @@ import java.util.List;
 public class CommentServiceImpl implements CommentService{
 
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final GameRepository gameRepository;
 
     @Override
     public List<CommentRespDto> findCommentByGameId(Long gameId) {
@@ -27,6 +34,36 @@ public class CommentServiceImpl implements CommentService{
             list.add(CommentRespDto.of(c));
         }
         return list;
+    }
+
+    @Override
+    public List<CommentRespDto> findCommentByMemberId(Long memberId) {
+        List<CommentRespDto> list = new ArrayList<>();
+        List<Comment> resp = commentRepository.findByMemberId(memberId);
+        for (Comment c : resp) {
+            log.info(c.getContent());
+            list.add(CommentRespDto.of(c));
+        }
+        return list;
+    }
+
+    @Override
+    public boolean saveComment(Long memberId, Long gameId, String content) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid member id"));
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid game id"));
+        if (game == null) {
+            throw new IllegalArgumentException("Invalid game id");
+        }
+        Comment comment = Comment.builder()
+                .member(member)
+                .game(game)
+                .content(content)
+                .createdAt(LocalDateTime.now())
+                .build();
+        commentRepository.save(comment);
+        return true;
     }
 
 }
