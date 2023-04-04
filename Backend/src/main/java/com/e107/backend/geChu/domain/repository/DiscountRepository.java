0d63ff405f648ad2bf59289bac085be73048f6e7
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-04-04 09:57:47 +0900

    discount add

---
+++ Backend/src/main/java/com/e107/backend/geChu/domain/repository/DiscountRepository.java
@@ -0,0 +1,7 @@
+package com.e107.backend.geChu.domain.repository;
+
+import com.e107.backend.geChu.domain.entity.Discount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscountRepository extends JpaRepository<Discount,Long> {
+}
