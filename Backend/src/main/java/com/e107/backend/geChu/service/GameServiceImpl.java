13bd2b92008fc8f7347e7dcb43e75cecad59a33a
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-04-04 14:11:13 +0900

    similarity test

--- Backend/src/main/java/com/e107/backend/geChu/service/GameServiceImpl.java
+++ Backend/src/main/java/com/e107/backend/geChu/service/GameServiceImpl.java
@@ -1,13 +1,16 @@
 package com.e107.backend.geChu.service;
 
 import com.e107.backend.geChu.domain.entity.Game;
+import com.e107.backend.geChu.domain.entity.Similarity;
 import com.e107.backend.geChu.domain.repository.GameRepository;
+import com.e107.backend.geChu.domain.repository.SimilarityRepository;
 import com.e107.backend.geChu.dto.response.GameDetailRespDto;
 import com.e107.backend.geChu.dto.response.GameListRespDto;
 import com.e107.backend.global.common.CommonException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +24,7 @@ import java.util.stream.Collectors;
 @Slf4j
 public class GameServiceImpl implements GameService {
     private final GameRepository gameRepository;
+    private final SimilarityRepository similarityRepository;
 
     @Override
     public GameDetailRespDto findGameByAppId(Long gameId) {
@@ -28,7 +32,6 @@ public class GameServiceImpl implements GameService {
         if (game == null) {
             throw new CommonException(2, "Game객체가 존재하지 않습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        
         return GameDetailRespDto.of(game);
     }
 
@@ -36,6 +39,16 @@ public class GameServiceImpl implements GameService {
     @Override
     public List<GameListRespDto> findAllGame(Pageable pageable) {
         return gameRepository.findAll(pageable).stream().map(GameListRespDto::of).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<GameListRespDto> findRecommendGame(Long gameId) {
+        Similarity s = similarityRepository.findById(gameId)
+                .orElseThrow(() -> new CommonException(2, "Game객체가 존재하지 않습니다.", HttpStatus.INTERNAL_SERVER_ERROR));
+        String similarity = s.getSimilarity();
+        log.info("similarity : " + similarity);
+
+        return null;
     }
 }
 
