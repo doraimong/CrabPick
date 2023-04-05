64ffba1ed799a5b12d6fdf9ed8e354128fd52242
WChan <baewc95@gmail.com>
2023-04-05 17:07:21 +0900

    [frontend] fix | comment

--- frontend/src/components/detail/Detail.tsx
+++ frontend/src/components/detail/Detail.tsx
@@ -200,47 +200,47 @@ const Detail = () => {
   }, [gameData]);
 
   /////////////////////////////코멘트////////////////////////////////////
-  const [commentRows, setCommentRows] = useState(1); // 현재 줄 수
-  const handleCommentChange = (event: any) => {
-    setCommentRows(event.target.value);
-    // 입력된 텍스트의 줄 수 체크
-    const rows = event.target.value.split("\n").length;
-    if (rows <= MAX_ROWS) {
-      setCommentRows(rows);
-    }
-  };
+  // const [commentRows, setCommentRows] = useState(1); // 현재 줄 수
+  // const handleCommentChange = (event: any) => {
+  //   setCommentRows(event.target.value);
+  //   // 입력된 텍스트의 줄 수 체크
+  //   const rows = event.target.value.split("\n").length;
+  //   if (rows <= MAX_ROWS) {
+  //     setCommentRows(rows);
+  //   }
+  // };
 
-  const handleKeyPress = (event: any) => {
-    // 현재 줄 수가 최대 줄 수와 같을 때 입력되지 않도록 처리
-    if (event.key === "Enter" && commentRows >= MAX_ROWS) {
-      event.preventDefault();
-    }
-  };
+  // const handleKeyPress = (event: any) => {
+  //   // 현재 줄 수가 최대 줄 수와 같을 때 입력되지 않도록 처리
+  //   if (event.key === "Enter" && commentRows >= MAX_ROWS) {
+  //     event.preventDefault();
+  //   }
+  // };
 
-  const [commentList, setCommentList] = useState<
-    { id: number; nickname: string; content: string }[]
-  >([]);
-  const [nextCommentId, setNextCommentId] = useState(1);
-  const submitComment = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const textarea = event.currentTarget.querySelector("textarea");
-    if (textarea) {
-      const commentText = textarea.value.trim();
-      if (commentText) {
-        setCommentList((prevList) => [
-          ...prevList,
-          { id: nextCommentId, nickname: "Guest", content: commentText },
-        ]);
-        setNextCommentId(nextCommentId + 1);
-        textarea.value = "";
-      }
-    }
-  };
-  const deleteComment = (id: number) => {
-    setCommentList((prevList) =>
-      prevList.filter((comment) => comment.id !== id)
-    );
-  };
+  // const [commentList, setCommentList] = useState<
+  //   { id: number; nickname: string; content: string }[]
+  // >([]);
+  // const [nextCommentId, setNextCommentId] = useState(1);
+  // const submitComment = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const textarea = event.currentTarget.querySelector("textarea");
+  //   if (textarea) {
+  //     const commentText = textarea.value.trim();
+  //     if (commentText) {
+  //       setCommentList((prevList) => [
+  //         ...prevList,
+  //         { id: nextCommentId, nickname: "Guest", content: commentText },
+  //       ]);
+  //       setNextCommentId(nextCommentId + 1);
+  //       textarea.value = "";
+  //     }
+  //   }
+  // };
+  // const deleteComment = (id: number) => {
+  //   setCommentList((prevList) =>
+  //     prevList.filter((comment) => comment.id !== id)
+  //   );
+  // };
 
   const steam = () => {
     window.open(`https://store.steampowered.com/app/${gameData?.appId}/`);
