6e77e00869a123e76c72f195b2f6246814ca4ab7
WChan <baewc95@gmail.com>
2023-04-07 08:56:24 +0900

    [frontend] comment
    Former-commit-id: c4e0278dfb6437ac73b7515146f694d6d3c9c213

--- frontend/src/components/detail/Detail.tsx
+++ frontend/src/components/detail/Detail.tsx
@@ -234,8 +234,8 @@ const Detail = () => {
   };
   const handleKeyPress = (event: any) => {
     // 현재 줄 수가 최대 줄 수와 같을 때 입력되지 않도록 처리
-    if (event.key === "Enter" && commentRows >= MAX_ROWS) {
-      event.preventDefault();
+    if (event.key === "Enter") {
+      submitComment
     }
   };
 
@@ -250,7 +250,6 @@ const Detail = () => {
       .get(`https://j8e107.p.ssafy.io/api/comment/${gameId}`)
       .then((response) => {
         setCommentList(response.data);
-        console.log('실행')
       });
   }, [nextCommentId]);
 
