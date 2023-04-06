52ec389465184fa474759fb9e5b9e4718ff339a2
leeseunghan <seunghan4769@pukyong.ac.kr>
2023-04-06 10:48:27 +0900

    서버 로그인
    Former-commit-id: 9b8dc594cc497e5f8a6a3d1fd0af99593e55795f

--- frontend/src/pages/SteamIdPage.tsx
+++ frontend/src/pages/SteamIdPage.tsx
@@ -25,11 +25,48 @@ const SteamIdPage = () => {
           // authCtx.userNickname = response.data.data.displayName;
           // authCtx.avatarfull = response.data.data.avatarfull;
           // console.log(authCtx.userId, authCtx.userNickname, authCtx.avatarfull);
+          const username = response.data.data.id;
+          const nickname = response.data.data._json.personaname;
           authCtx.login(
             response.data.data.id,
             response.data.data._json.personaname,
             response.data.data._json.avatarfull
           );
+          // 스팀에서 정보 받아왔으니 이제 우리 스프링에 로그인 요청
+          axios
+            .post("https://j8e107.p.ssafy.io/api/authenticate", {
+              username: username,
+              password: "password",
+            })
+            .then((res) => {
+              console.log(res.data);
+              sessionStorage.setItem("token", res.data.token);
+              sessionStorage.setItem("memberId", res.data.memberId);
+            })
+            // 로그인 실패다? 그럼 처음 오는 거임 일단 회원가입 강제로 시켜버리기
+            .catch(() => {
+              let userDto = {
+                username: username,
+                password: "password",
+                nickname: nickname,
+              };
+              // 회원가입 요청
+              axios
+                .post("https://j8e107.p.ssafy.io/api/signup", userDto)
+                .then((res) => {
+                  // 회원가입 하자마자 바로 로그인 시키기
+                  console.log(res.data);
+                  axios
+                    .post("https://j8e107.p.ssafy.io/api/authenticate", {
+                      username: username,
+                      password: "password",
+                    })
+                    .then((res) => {
+                      sessionStorage.setItem("token", res.data.token);
+                      sessionStorage.setItem("memberId", res.data.memberId);
+                    });
+                });
+            });
         } else {
           console.log("밑에는 두번째 보내는 겁니다.");
         }
