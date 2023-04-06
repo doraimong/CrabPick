2ca79fb771cd87ab83d7b3e68504e725344bc69f
WChan <baewc95@gmail.com>
2023-04-06 11:55:53 +0900

    [frontend] search
    Former-commit-id: 11d1f3c5042461e87d5a2978576b287eb619a6c3

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DetailLayout from "../layout/DetailLayout";
 import Detail from "../components/detail/Detail";
 
@@ -8,79 +8,91 @@ import SearchResult from "../components/search/SearchResult";
 import { useLocation } from "react-router-dom";
 
 import image from "../asset/ddd.png";
+import axios from "axios";
 
 const SearchPage = () => {
   const { state } = useLocation();
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
-  const pageNumbersToShow = 5;
+  const [currentItems, setCurrentItems] = useState<any>([]);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const itemsPerPage = 10;
+  // const pageNumbersToShow = 5;
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = state.filteredGameList.slice(
-    indexOfFirstItem,
-    indexOfLastItem
-  );
+  // const indexOfLastItem = currentPage * itemsPerPage;
+  // const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
-  const totalPages = Math.ceil(state.filteredGameList.length / itemsPerPage);
+  useEffect(() => {
+    axios
+      .get(`https://j8e107.p.ssafy.io/api/game/name/${state.searchInput}`)
+      .then((res) => {
+        setCurrentItems(res.data);
+      })
+      .catch((e) => {});
+  }, [state.searchInput]);
 
-  const handleClickPageNumber = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-    window.scrollTo(0, 0);
-  };
+  // .slice(
+  //   indexOfFirstItem,
+  //   indexOfLastItem
+  // );
 
-  const handleGoToFirstPage = () => {
-    setCurrentPage(1);
-    window.scrollTo(0, 0);
-  };
+  // const totalPages = Math.ceil(state.filteredGameList.length / itemsPerPage);
 
-  const handleGoToLastPage = () => {
-    setCurrentPage(totalPages);
-    window.scrollTo(0, 0);
-  };
+  // const handleClickPageNumber = (pageNumber: number) => {
+  //   setCurrentPage(pageNumber);
+  //   window.scrollTo(0, 0);
+  // };
 
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-      window.scrollTo(0, 0);
-    }
-  };
+  // const handleGoToFirstPage = () => {
+  //   setCurrentPage(1);
+  //   window.scrollTo(0, 0);
+  // };
 
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-      window.scrollTo(0, 0);
-    }
-  };
+  // const handleGoToLastPage = () => {
+  //   setCurrentPage(totalPages);
+  //   window.scrollTo(0, 0);
+  // };
 
-  const pageNumbers = [];
-  if (totalPages <= pageNumbersToShow) {
-    for (let i = 1; i <= totalPages; i++) {
-      pageNumbers.push(i);
-    }
-  } else {
-    const halfPageNumbersToShow = Math.floor(pageNumbersToShow / 2);
-    let startPageNumber = currentPage - halfPageNumbersToShow;
-    let endPageNumber = currentPage + halfPageNumbersToShow;
+  // const handleNextPage = () => {
+  //   if (currentPage < totalPages) {
+  //     setCurrentPage(currentPage + 1);
+  //     window.scrollTo(0, 0);
+  //   }
+  // };
 
-    if (startPageNumber < 1) {
-      endPageNumber += Math.abs(startPageNumber) + 1;
-      startPageNumber = 1;
-    } else if (endPageNumber > totalPages) {
-      startPageNumber -= endPageNumber - totalPages;
-      endPageNumber = totalPages;
-    }
+  // const handlePrevPage = () => {
+  //   if (currentPage > 1) {
+  //     setCurrentPage(currentPage - 1);
+  //     window.scrollTo(0, 0);
+  //   }
+  // };
 
-    for (let i = startPageNumber; i <= endPageNumber; i++) {
-      pageNumbers.push(i);
-    }
-  }
+  // const pageNumbers = [];
+  // if (totalPages <= pageNumbersToShow) {
+  //   for (let i = 1; i <= totalPages; i++) {
+  //     pageNumbers.push(i);
+  //   }
+  // } else {
+  //   const halfPageNumbersToShow = Math.floor(pageNumbersToShow / 2);
+  //   let startPageNumber = currentPage - halfPageNumbersToShow;
+  //   let endPageNumber = currentPage + halfPageNumbersToShow;
+
+  //   if (startPageNumber < 1) {
+  //     endPageNumber += Math.abs(startPageNumber) + 1;
+  //     startPageNumber = 1;
+  //   } else if (endPageNumber > totalPages) {
+  //     startPageNumber -= endPageNumber - totalPages;
+  //     endPageNumber = totalPages;
+  //   }
+
+  //   for (let i = startPageNumber; i <= endPageNumber; i++) {
+  //     pageNumbers.push(i);
+  //   }
+  // }
 
   return (
     <SearchLayout>
       <h3 className={styles.SearchInput}>검색 : {state.searchInput}</h3>
       <div className={styles.SearchPage}>
-        {currentItems.length !== 0 ? (
+        {currentItems && currentItems.length !== 0 ? (
           currentItems.map((game: any, i: number) => (
             <SearchResult key={i} game={game} />
           ))
@@ -94,7 +106,7 @@ const SearchPage = () => {
             </div>
           </div>
         )}
-        <div className={styles.pageButton}>
+        {/* <div className={styles.pageButton}>
           <button onClick={handleGoToFirstPage}>First</button>
           <button onClick={handlePrevPage}>Prev</button>
           <div className={styles.pagenation}>
@@ -110,7 +122,7 @@ const SearchPage = () => {
           </div>
           <button onClick={handleNextPage}>Next</button>
           <button onClick={handleGoToLastPage}>Last</button>
-        </div>
+        </div> */}
       </div>
     </SearchLayout>
   );
