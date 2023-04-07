af57128d1f4fd6a71f848e188ea4e060ecd63086
leeseunghan <seunghan4769@pukyong.ac.kr>
2023-04-07 09:25:00 +0900

    보유게임
    Former-commit-id: 33de07ec3971b7a9b5efc4309bac4ec01c57366b

--- frontend/src/components/detail/Detail.tsx
+++ frontend/src/components/detail/Detail.tsx
@@ -235,7 +235,7 @@ const Detail = () => {
   const handleKeyPress = (event: any) => {
     // 현재 줄 수가 최대 줄 수와 같을 때 입력되지 않도록 처리
     if (event.key === "Enter") {
-      submitComment
+      submitComment(event.target.value);
     }
   };
 
